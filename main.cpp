726914954c9415970e9a60e4f8a6f0142ca9325d
Ben Baker <bakerb845@gmail.com>
2019-05-09 19:23:29 -0600

    screwing around with more complicated qt example

--- main.cpp
+++ main.cpp
@@ -1,14 +1,34 @@
 #include <QApplication>
-#include <QPushButton>
+#include <QStyleHints>
+#include <QDesktopWidget>
+#include <QTranslator>
+#include <QLocale>
+#include <QLibraryInfo>
 
-int main(int argc, char **argv)
+#include "dialog.h"
+
+int main(int argc, char *argv[])
 {
- QApplication app (argc, argv);
+    QCoreApplication::setAttribute(Qt::AA_UseHighDpiPixmaps);
+    QApplication app(argc, argv);
+    QGuiApplication::setApplicationDisplayName(Dialog::tr("Standard Dialogs"));
 
- QPushButton button;
- button.setText("My text");
- button.setToolTip("A tooltip");
- button.show();
+#ifndef QT_NO_TRANSLATION
+    QString translatorFileName = QLatin1String("qt_");
+    translatorFileName += QLocale::system().name();
+    QTranslator *translator = new QTranslator(&app);
+    if (translator->load(translatorFileName, QLibraryInfo::location(QLibraryInfo::TranslationsPath)))
+        app.installTranslator(translator);
+#endif
 
- return app.exec();
+    Dialog dialog;
+    if (!QGuiApplication::styleHints()->showIsFullScreen() && !QGuiApplication::styleHints()->showIsMaximized()) {
+        const QRect availableGeometry = QApplication::desktop()->availableGeometry(&dialog);
+        dialog.resize(availableGeometry.width() / 3, availableGeometry.height() * 2 / 3);
+        dialog.move((availableGeometry.width() - dialog.width()) / 2,
+                    (availableGeometry.height() - dialog.height()) / 2);
+    }
+    dialog.show();
+
+    return app.exec();
 }
