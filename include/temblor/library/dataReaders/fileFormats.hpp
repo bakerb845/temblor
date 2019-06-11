fbc2aeedfc1ea01d8e231e955289ce12965f9f85
Ben Baker <bakerb845@gmail.com>
2019-06-11 11:18:48 -0600

    adding something to keep track of file formats

---
+++ include/temblor/library/dataReaders/fileFormats.hpp
@@ -0,0 +1,16 @@
+#ifndef TEMBLOR_LIBRARY_DATAREADERS_FILEFORMATS_HPP
+#define TEMBLOR_LIBRARY_DATAREADERS_FILEFORMATS_HPP 1
+
+namespace Temblor::Library::DataReaders
+{
+/*!
+ * @brief Supported seismic data file formats.
+ */
+enum class FileFormatTypes
+{
+    SAC  /*!< SAC file format. */
+};
+
+}
+
+#endif
